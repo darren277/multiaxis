3916e69fcb0c91c3d83b3fa7048459401aa1bad0
darren277 <darren277@yahoo.com>
2025-04-10 05:09:44 -0700

    Example scene items for adventure presentation.

---
+++ src/drawing/sceneItems.js
@@ -0,0 +1,35 @@
+// Example: 10 items placed around the scene
+const SCENE_ITEMS = [
+    {
+        id: "photo1",
+        position: { x: 0, y: 2, z: 0 },
+        //image: "step1.jpg",
+        image: "textures/Canestra_di_frutta_Caravaggio.jpg",
+        caption: "<h2>Photo 1</h2><p>Captivating forest scene!</p>",
+    },
+    {
+        id: "photo2",
+        position: { x: 5, y: 3, z: -5 },
+        //image: "step2.jpg",
+        image: "textures/Canestra_di_frutta_Caravaggio.jpg",
+        caption: "<h2>Photo 2</h2><p>A beautiful mountain peak.</p>",
+    },
+    {
+        id: "photo3",
+        position: { x: -5, y: 2, z: 10 },
+        //image: "step3.jpg",
+        image: "textures/Canestra_di_frutta_Caravaggio.jpg",
+        caption: "<h2>Photo 3</h2><p>Riverside cottage at sunset.</p>",
+    },
+    // ... up to photo10 ...
+    {
+        id: "photo10",
+        position: { x: 12, y: 1, z: 8 },
+        //image: "step10.jpg",
+        image: "textures/Canestra_di_frutta_Caravaggio.jpg",
+        caption: "<h2>Photo 10</h2><p>Hidden waterfall in the forest.</p>",
+    },
+];
+
+
+export { SCENE_ITEMS };
