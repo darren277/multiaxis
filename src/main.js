1a45de82d53e8f8d1a750317f3bb5036c2794249
darren277 <darren277@yahoo.com>
2025-04-26 07:10:16 -0700

    Modified the panoramic cube background rendering.

--- src/main.js
+++ src/main.js
@@ -237,7 +237,8 @@ async function contentLoadedCallback(threejsDrawing) {
     }
 
     // --- OPTION 1: Panoramic cube skybox ---
-    //usePanoramicCubeBackground(scene);
+    //usePanoramicCubeBackground(scene, 'textures/sun_temple_stripe.jpg');
+    //usePanoramicCubeBackgroundSixFaces(scene, 'textures/exr/golden_gate_hills_1k');
 
     // --- OPTION 2: Simple procedural background ---
     /////useProceduralBackground(scene);
