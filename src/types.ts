0c8fec32b79c552af82ed73158f045dcd6da2a55
darren277 <darren277@yahoo.com>
2025-06-12 11:19:14 -0700

    Created QueryOptions and SceneConfig types.

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,29 @@
 import type { ThreeJSDrawing } from './threejsDrawing.d.ts';
 
+type QueryOptions = {
+    debug?: boolean;
+    controls?: string;
+    prev?: string;
+    nav?: boolean;
+    [key: string]: any;
+};
+
+type SceneConfig = {
+    startPosition?: { x: number, y: number, z: number },
+    lookAt?: { x: number, y: number, z: number },
+    clippingPlane?: number,
+    background?: number | string,
+    controller?: 'none' | 'orbital' | 'walking' | 'pointerlock' | 'trackball',
+    cssRendererEnabled?: boolean | '2D' | '3D' | 'DUAL',
+    statsEnabled?: boolean,
+    vrEnabled?: boolean
+};
+
 export {
     ThreeJSDrawing
-}
+};
+
+export type {
+    QueryOptions,
+    SceneConfig
+};
