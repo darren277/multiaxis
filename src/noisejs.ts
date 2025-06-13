bfbe603f410592d7616857dddeadf10745d84a4e
darren277 <darren277@yahoo.com>
2025-06-13 15:22:57 -0700

    Noisejs declaration file for Typescript.

---
+++ src/noisejs.ts
@@ -0,0 +1,23 @@
+
+declare module "noisejs" {
+    export class Noise {
+        constructor(seed: number);
+        perlin2(x: number, y: number): number;
+        simplex2(x: number, y: number): number;
+    }
+    export class Perlin {
+        constructor(seed: number);
+        noise(x: number, y: number): number;
+        noise3D(x: number, y: number, z: number): number;
+    }
+    export class Simplex {
+        constructor(seed: number);
+        noise(x: number, y: number): number;
+        noise3D(x: number, y: number, z: number): number;
+    }
+    export class Fbm {
+        constructor(seed: number, octaves: number);
+        noise(x: number, y: number): number;
+        noise3D(x: number, y: number, z: number): number;
+    }
+}
