5d93694674df9b6f3ec73fe2ae7c390674e1d9b7
darren277 <darren277@yahoo.com>
2025-06-20 15:22:10 -0700

    Code formatting and linting for main files.

--- src/perlin-noise.ts
+++ src/perlin-noise.ts
@@ -1,10 +1,13 @@
-
-declare module "perlin-noise" {
+declare module 'perlin-noise' {
     export type PerlinNoiseOptions = {
-        octaveCount?: number;
-        amplitude?: number;
-        persistence?: number;
-    };
-    export function generatePerlinNoise(width: number, height: number, options?: PerlinNoiseOptions): number[];
-    export function generateWhiteNoise(width: number, height: number): number[];
+        octaveCount?: number
+        amplitude?: number
+        persistence?: number
+    }
+    export function generatePerlinNoise(
+        width: number,
+        height: number,
+        options?: PerlinNoiseOptions,
+    ): number[]
+    export function generateWhiteNoise(width: number, height: number): number[]
 }
--- src/types.ts
+++ src/types.ts
@@ -1,61 +1,55 @@
-import type { ThreeJSDrawing } from './threejsDrawing.d.ts';
-import type { ALL_CUBE_DEFS } from './config/navigation';
+import type { ThreeJSDrawing } from './threejsDrawing.d.ts'
+import type { ALL_CUBE_DEFS } from './config/navigation'
 
 type QueryOptions = {
-    debug?: boolean;
-    controls?: string;
-    prev?: string;
-    nav?: boolean;
-    [key: string]: any;
-};
+    debug?: boolean
+    controls?: string
+    prev?: string
+    nav?: boolean
+    [key: string]: any
+}
 
 type SceneConfig = {
-    startPosition?: { x: number, y: number, z: number },
-    lookAt?: { x: number, y: number, z: number },
-    clippingPlane?: number,
-    background?: number | string,
-    controller?: 'none' | 'orbital' | 'walking' | 'pointerlock' | 'trackball',
-    cssRendererEnabled?: boolean | '2D' | '3D' | 'DUAL',
-    statsEnabled?: boolean,
+    startPosition?: { x: number; y: number; z: number }
+    lookAt?: { x: number; y: number; z: number }
+    clippingPlane?: number
+    background?: number | string
+    controller?: 'none' | 'orbital' | 'walking' | 'pointerlock' | 'trackball'
+    cssRendererEnabled?: boolean | '2D' | '3D' | 'DUAL'
+    statsEnabled?: boolean
     vrEnabled?: boolean
-};
+}
 
 // TODO: Define a few of these more precisely.
 // Some drawings share the same attributes while some other do not.
 type DrawingData = {
-    staticBoxes?: any[];
-    movingMeshes?: any[];
-    obstacleBoxes?: any[];
-    worldMeshes?: any[];
-    collision?: any;
-    keyManager?: any;
-    [key: string]: any;
-};
+    staticBoxes?: any[]
+    movingMeshes?: any[]
+    obstacleBoxes?: any[]
+    worldMeshes?: any[]
+    collision?: any
+    keyManager?: any
+    [key: string]: any
+}
 
 export interface ThreeJSDrawingEventContext {
-    camera: any;
-    data: DrawingData;
-    controls?: any;
-    renderer: any;
-    scene: any;
+    camera: any
+    data: DrawingData
+    controls?: any
+    renderer: any
+    scene: any
 }
 
 export interface ThreeJSDrawingsMap {
     [drawingName: string]: {
-        drawing: ThreeJSDrawing;
-        data: DrawingData;
-        sceneConfig: SceneConfig;
-    };
+        drawing: ThreeJSDrawing
+        data: DrawingData
+        sceneConfig: SceneConfig
+    }
 }
 
-export {
-    ThreeJSDrawing,
-};
+export { ThreeJSDrawing }
 
-export type {
-    QueryOptions,
-    SceneConfig,
-    DrawingData,
-};
+export type { QueryOptions, SceneConfig, DrawingData }
 
-export type ALL_CUBE_DEFS = { [key: string]: any };
+export type ALL_CUBE_DEFS = { [key: string]: any }
