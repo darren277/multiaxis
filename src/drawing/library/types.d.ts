5b823a598f585c3722346ebc1f2eb2439bcb61b2
darren277 <darren277@yahoo.com>
2025-06-20 15:26:11 -0700

    Code formatting and linting for library files.

--- src/drawing/library/types.d.ts
+++ src/drawing/library/types.d.ts
@@ -1,24 +1,24 @@
 type Resource = {
-    name: string;
-    author: string;
-    year: number;
-    dewey: string;
-    word_count: number;
-    personal_rating: number;
-    description: string;
+    name: string
+    author: string
+    year: number
+    dewey: string
+    word_count: number
+    personal_rating: number
+    description: string
 }
 
 type Library = {
     bookcase: {
-        width: number;
-        height: number;
-        depth: number;
-    };
-    numberOfRows: number;
-    numberOfCases: number;
-    spaceBetweenRows: number;
-    spaceBetweenCases: number;
-    numberOfFloors: number;
+        width: number
+        height: number
+        depth: number
+    }
+    numberOfRows: number
+    numberOfCases: number
+    spaceBetweenRows: number
+    spaceBetweenCases: number
+    numberOfFloors: number
 }
 
-export type { Resource, Library };
+export type { Resource, Library }
